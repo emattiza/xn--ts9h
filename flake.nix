46fafd1b2aa5259f33fcd42add7fa10fd9ddfddf
emattiza <emattiza@gmail.com>
2023-01-20 10:32:44 -0600

    working flake template added

--- flake.nix
+++ flake.nix
@@ -2,14 +2,41 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
+    nci = {
+      url = "github:yusdacra/nix-cargo-integration";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, utils }:
-    utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [ rustc cargo rustfmt rust-analyzer ed ];
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+    nci,
+    ...
+  }: let
+    outputs = nci.lib.makeOutputs {
+      root = ./.;
+      config = common: {
+        outputs.defaults = {
+          app = "xn--ts9h";
+          package = "xn--ts9h";
         };
-      });
+      };
+      overrides = {
+        shell = common: prev: {
+          packages =
+            prev.packages
+            ++ [
+              common.pkgs.rust-analyzer
+              common.pkgs.rustfmt
+            ];
+        };
+      };
+    };
+  in
+    outputs
+    // {
+      inherit (outputs) apps devShells;
+    };
 }
